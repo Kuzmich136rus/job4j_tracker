3887c0836b523c4865dfa00bfcec13071d98e735
Kuzmich36rus <sledak-vrn@yandex.ru>
2022-07-03 21:08:20 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,37 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean code", 464);
+        Book headFirst = new Book("Head First Java", 635);
+        Book philosophy = new Book("Филосовия Java", 1168);
+        Book library = new Book("Библиотека профессионала", 864);
+
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = headFirst;
+        books[2] = philosophy;
+        books[3] = library;
+        System.out.println("Вывод на консоль списка книг: ");
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+        System.out.println("Вывод на консоль списка после перемены местами 0 и 3 индекса: ");
+        Book temp;
+        temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+        System.out.println("Вывод на консоль книги Cnean code: ");
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + " - " + bk.getPages());
+            }
+        }
+    }
+}
